50e9f2e03dac3c8603abe0b904641067105c6bf7
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-08-03 01:31:40 -0600

    finished product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // finds all products
+  Product.findAll ({
+    attributes: ['id','category_id','price','stock','product_name'],
+  // includes its associated Category and Tag data
+  include: [
+    {
+      model: Category,
+      attributes: ['id','category_name'],
+    },
+    {
+      model: Tag,
+      attributes: ['id', 'tag_name'],
+      through: ProductTag,
+      as: 'tags'
+    },
+  ]
+  })
+  .then(dbProductData => res.json(dbProductData))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne ({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id','category_id','price','stock','product_name'],
   // be sure to include its associated Category and Tag data
+  include: [
+    {
+      model: Category,
+      attributes: ['id', 'category_name'],
+    },
+    {
+      model: Tag,
+      attributes: ['id', 'tag_name'],
+      through: ProductTag,
+      as: 'tags'
+    },
+  ]
+})
+  .then(dbProductData => res.json(dbProductData))
 });
 
 // create new product
@@ -25,7 +60,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create(req.body)({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tag_id,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
