44f1331dbad661692128e40455764b98ad765a82
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-08-02 23:11:52 -0600

    finished updating the models with the right id info and categorical information

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,8 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { values } = require('sequelize/types/lib/operators');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,47 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+    product_name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        customValidator(value) {
+          if (value !== DECIMAL) {
+            throw new Error ("please enter a proper decimal value");
+          }
+        }
+      }
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      set: values.INTEGER,
+      defaultValue: 10,
+      validate: {
+        customValidator(value) {
+          if (value === null && value !== INTEGER) {
+            throw new Error("please enter a numeric value");
+          }
+        }
+      }
+    },
+    category_id: {
+      type:DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
